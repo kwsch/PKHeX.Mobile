27af0a0ba17a22836e2d1af749bc701157516021
brandonjmarquez <60440397+brandonjmarquez@users.noreply.github.com>
2024-01-31 16:12:52 -0600

    Added AppCenter Plugins

--- PKHeX.Mobile/App.xaml.cs
+++ PKHeX.Mobile/App.xaml.cs
@@ -20,11 +20,11 @@ namespace PKHeX
             // Handle when your app starts
             Licensing.LoadLicenses();
             GameInfo.Strings = GameInfo.GetStrings(GameInfo.CurrentLanguage);
-            AppCenter.Start("ios={Your App secret here};" +
+            AppCenter.Start("ios=6696eefb-f004-4303-963d-72a3fe9a8061;" +
                   "uwp={Your UWP App secret here};" +
                   "android={Your Android App secret here};" +
                   "macos={Your macOS App secret here};",
-                  typeof(Analytics), typeof(Crashes))
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
